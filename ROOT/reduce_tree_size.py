73f9743f8bf57c2d147e3365729c92a5c05d2203
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-08-05 18:58:52 +0200

    :bug: fixes for running successfully

--- ROOT/reduce_tree_size.py
+++ ROOT/reduce_tree_size.py
@@ -1,14 +1,13 @@
 import os
+import argparse
 import ROOT
 
 def reduce_tree_size(infile, tname, nentries):
     f_old = ROOT.TFile.Open(infile, 'READ')
     t_old = f_old.Get(tname)
 
-    dirname = os.path.dirname(infile)
-    basename = os.path.basename(infile)
-    base, ext = d.split('.')
-    outfile = os.path.join(dirname, base + '_reduced' + ext)
+    base, ext = infile.split('.')
+    outfile = base + '_reduced.' + ext
 
     f_new = ROOT.TFile(outfile, 'RECREATE')
     t_new = t_old.CloneTree(0)
@@ -16,22 +15,22 @@ def reduce_tree_size(infile, tname, nentries):
     for entry in range(0,t_old.GetEntries()):
         t_old.GetEntry(entry)
         t_new.Fill()
-        if entry > netries:
+        if entry > nentries:
             break
      
     f_new.Write()
     print('Skimmed tree written at {}.'.format(outfile))
 
 if __name__ == '__main__':
-    usage = '\n'.join('Reduces the number of entries in a TTree.',
-                      'Keeps the first `n` entries.')
+    usage = '\n'.join(('Reduces the number of entries in a TTree.',
+                       'Keeps the first `n` entries.'))
     parser = argparse.ArgumentParser(description=usage)
     parser.add_argument('-i', '--infile', dest='infile',
                         required=True, help='input file')
     parser.add_argument('-t', '--treename', dest='tname',
                         required=True, help='name of the TTree')
     parser.add_argument('-n', '--nentries', dest='nentries',
-                        default=100, help='number of entries to be kept')
+                        type=int, default=100, help='number of entries to be kept')
     FLAGS = parser.parse_args()
 
-    reduce_tree_size(FLAGS.infile, FLAGS.tname, FLAGS.nentries):
+    reduce_tree_size(FLAGS.infile, FLAGS.tname, FLAGS.nentries)
