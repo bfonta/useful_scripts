ba7982c3dede06f81c67fb580e8bba833c0a038f
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-09-08 14:57:11 +0200

    add calculation directly with binomial

--- Statistics/ClopperPearsonConfidence.py
+++ Statistics/ClopperPearsonConfidence.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.stats import beta
+from scipy.stats import beta, binom
 
 k = [845, 8875]
 N = [895, 161199]
@@ -8,6 +8,8 @@ alpha = 0.05
 significance = alpha/2
 confidence = 1-alpha
 
+# using definition with beta functions
+print('---- Beta ----')
 for valk,valN,vale in zip(k,N,eff):
        beta_lo = beta(valk,valN-valk+1)
        beta_hi = beta(valk+1, valN-valk)
@@ -18,3 +20,14 @@ for valk,valN,vale in zip(k,N,eff):
        print('High: ', quant_hi)
        print()
 
+# using definition with binomials
+print('---- Binomial ----')
+for valk,valN,vale in zip(k,N,eff):
+    binomial = binom(valN, vale)
+    quant_lo = binomial.ppf(significance)
+    quant_hi = binomial.ppf(1-significance)
+    print('Low: ', quant_lo/valN)
+    print('Eff: ', vale)
+    print('High: ', quant_hi/valN)
+    print()
+    
