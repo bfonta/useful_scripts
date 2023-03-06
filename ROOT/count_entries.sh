4eb09952c09091fb9afe5545013ef155f2d2d4fe
Bruno Alves <balves16@hotmail.com>
2023-03-06 10:36:31 +0100

    fix script name

--- ROOT/count_entries.sh
+++ ROOT/count_entries.sh
@@ -13,8 +13,8 @@ data_folder="/data_CMS/cms/alves/HHresonant_SKIMS/SKIMS_UL18_7Feb2023"
 for adir in ${data_folder}/*/; do
     eos_type=`basename ${adir}`
     for afile in ${adir}/*root; do
-	n1=$(python3 ndir.py -i ${afile} -t HTauTauTree)
-	n2=$(python3 ndir.py -i ${eos_folder}${eos_type}/$(basename $afile) -t HTauTauTree)
+	n1=$(python3 count_entries.py -i ${afile} -t HTauTauTree)
+	n2=$(python3 count_entries.py -i ${eos_folder}${eos_type}/$(basename $afile) -t HTauTauTree)
 	if [[ $n1 -ne $n2 ]]; then 
 	    echo $n1 $n2 ${afile} ${eos_folder}${eos_type}/$(basename $afile)
 	fi
