f6ac43697fb6a29d77ab3705c6ba82cd4a77e0b8
Bruno Alves <balves16@hotmail.com>
2023-08-03 10:11:52 +0200

    update ranges according to Xanda's suggestion

--- Physics/generateCardsFromTemplate/generateCards.py
+++ Physics/generateCardsFromTemplate/generateCards.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+65;7200;1c#!/usr/bin/python
 
 import sys
 import os
@@ -207,10 +207,9 @@ def main(opt):
     cont = CardsContent(dir_template, template_name)
 
     ## list of parameters being scanned
-    mass_points = (250, 260, 270, 280, 290, 300, 320, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800,
-                   850, 900, 950, 1000)
+    mass_points = (280, 300, 400, 500, 600, 700, 800)
     stheta_points = np.arange(0.,1.001,.1) # sine of theta mixing between the new scalar and the SM Higgs
-    l112_points = np.arange(-300,301,100) # resonance coupling with two Higgses
+    l112_points = np.arange(-600,601,100) # resonance coupling with two Higgses
     lambda111_sm = np.round(125**2 / (2*246.), 6) # tri-linear Higgs coupling
     k111_points = (1.0, 2.4, 10.0) #np.arange(-7,12) # tri-linear kappa
 
